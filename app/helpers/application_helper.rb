2729d1cbdcb57186db22aed6d80d82f2087462c9
Shiv <shiv.yuvasoft@gmail.com>
2017-04-13 19:36:15 +0530

    fixed missing image and search category

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,15 @@
 module ApplicationHelper
   def avatar_url(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-    if user.image
+    if user.image.present?
       user.image
     else
-      ""
+      if Rails.env.eql? "production"
+        default_url = "#{rails_root}images/user_default_avatar.jpg"
+      else
+        default_url = "https://thumb.ibb.co/jBGmFk/logo.png"  
+      end
+      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+      url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
     end
   end
 
