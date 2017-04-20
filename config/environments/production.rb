acdb243b36fe12e99b91a6c4d93e077f8423c6ab
Sumit <sumit.yuvasoft131@gmail.com>
2017-04-20 16:43:41 +0530

    Added the madrill smtp setting and host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+   config.action_mailer.default_url_options = { host: 'https://airbnb-app1.herokuapp.com/'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mandrillapp.com',
+    port: 587,
+    enable_starttls_auto: true,
+    user_name: 'shopsalud',
+    password: 'ZV50V-a3tJkrOV7UDT9Xig',
+    authentication: 'login'
+  }
 end
