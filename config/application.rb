ce6de8c33a0c844e19516c44b42dd95e57bbab78
ambey99 <ambeygupta01@gmail.com>
2017-04-24 13:08:15 +0530

    Chnaged the application file

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module AirAlien
   class Application < Rails::Application
     config.middleware.delete Rack::Lock
-    config.middleware.use FayeRails::Middleware, mount: '/faye', :timeout => 25
+    config.middleware.use FayeRails::Middleware, mount: '/faye', :timeout => 2500
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
