dbf6cd76e60d24783178f4be5585a4f969fb3528
anand <anandyuvasoft190@gmail.com>
2017-05-01 19:31:49 +0530

    minor changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -40,7 +40,7 @@ $(function(){
 
 
   //function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {center: {lat: -33.8688, lng: 151.2195},zoom: 13});
+  var map = new google.maps.Map(document.getElementById('pac-input'), {center: {lat: -33.8688, lng: 151.2195},zoom: 13});
   var input = document.getElementById('pac-input');
   var autocomplete = new google.maps.places.Autocomplete(input);
   //}
