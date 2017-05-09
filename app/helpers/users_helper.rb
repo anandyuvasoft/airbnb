73d7502b099ab25328e7684ed283731445a53a75
Anand <anandyuvasoft190@gmail.com>
2017-05-09 13:02:40 +0530

    edit relatives

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+
+  def get_relative_object_helper
+    object = params[:relative_id].present? ? Relative.find(params[:relative_id]) : current_patient.relatives.build
+  end
+
 end
