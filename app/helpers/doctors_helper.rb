692a4265f61217bc9db695f49409ba6a266d9ea7
Anand <anandyuvasoft190@gmail.com>
2017-05-15 17:26:46 +0530

    to do list

--- app/helpers/doctors_helper.rb
+++ app/helpers/doctors_helper.rb
@@ -1,2 +1,16 @@
 module DoctorsHelper
+
+  def is_room_approved
+    current_doctor.rooms.where(is_admin_active: true).present? ? true : false 
+  end
+
+  def is_friend_refered
+    current_doctor.friends.present? ? true : false 
+  end
+
+  def is_upgrade
+    current_doctor.purchases.present? ? true : false
+  end 
+
+
 end
